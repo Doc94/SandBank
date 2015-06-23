963668e889c6cbbb6daa6beed4843d1b59775c96
DavidDeFilippi <d.defilippi@asistenciapcmaipu.cl>
2015-06-23 16:12:24 -0300

    Diseño de ventana login
    se ha insertado logo e icono y se cambio color de fondo y fuente de
    labels y botones

--- Sandbank/Form1.Designer.cs
+++ Sandbank/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Sandbank
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -28,50 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.textBox_rut = new System.Windows.Forms.TextBox();
             this.textBox_password = new System.Windows.Forms.TextBox();
             this.button_ingresar = new System.Windows.Forms.Button();
             this.button_cancelar = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 36);
+            this.label1.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(43, 13);
+            this.label1.Size = new System.Drawing.Size(51, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Usuario";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(16, 73);
+            this.label2.Font = new System.Drawing.Font("Corbel", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 121);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.Size = new System.Drawing.Size(73, 15);
             this.label2.TabIndex = 1;
             this.label2.Text = "Contraseña";
             // 
             // textBox_rut
             // 
-            this.textBox_rut.Location = new System.Drawing.Point(85, 36);
+            this.textBox_rut.Location = new System.Drawing.Point(85, 86);
             this.textBox_rut.Name = "textBox_rut";
-            this.textBox_rut.Size = new System.Drawing.Size(100, 20);
+            this.textBox_rut.Size = new System.Drawing.Size(156, 20);
             this.textBox_rut.TabIndex = 2;
             // 
             // textBox_password
             // 
-            this.textBox_password.Location = new System.Drawing.Point(85, 73);
+            this.textBox_password.Location = new System.Drawing.Point(85, 119);
             this.textBox_password.Name = "textBox_password";
-            this.textBox_password.Size = new System.Drawing.Size(100, 20);
+            this.textBox_password.Size = new System.Drawing.Size(156, 20);
             this.textBox_password.TabIndex = 3;
             this.textBox_password.UseSystemPasswordChar = true;
             // 
             // button_ingresar
             // 
-            this.button_ingresar.Location = new System.Drawing.Point(65, 125);
+            this.button_ingresar.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_ingresar.Location = new System.Drawing.Point(85, 161);
             this.button_ingresar.Name = "button_ingresar";
             this.button_ingresar.Size = new System.Drawing.Size(75, 23);
             this.button_ingresar.TabIndex = 4;
@@ -81,26 +87,42 @@
             // 
             // button_cancelar
             // 
-            this.button_cancelar.Location = new System.Drawing.Point(159, 125);
+            this.button_cancelar.Font = new System.Drawing.Font("Corbel", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_cancelar.Location = new System.Drawing.Point(166, 161);
             this.button_cancelar.Name = "button_cancelar";
             this.button_cancelar.Size = new System.Drawing.Size(75, 23);
             this.button_cancelar.TabIndex = 5;
             this.button_cancelar.Text = "Cerrar";
             this.button_cancelar.UseVisualStyleBackColor = true;
+            this.button_cancelar.Click += new System.EventHandler(this.button_cancelar_Click);
             // 
-            // Form1
+            // pictureBox1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(294, 182);
+            this.pictureBox1.Image = global::Sandbank.Properties.Resources.logo;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(258, 54);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(291, 211);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button_cancelar);
             this.Controls.Add(this.button_ingresar);
             this.Controls.Add(this.textBox_password);
             this.Controls.Add(this.textBox_rut);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Name = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
             this.Text = "Iniciar sesión";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -114,6 +136,7 @@
         private System.Windows.Forms.TextBox textBox_password;
         private System.Windows.Forms.Button button_ingresar;
         private System.Windows.Forms.Button button_cancelar;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
